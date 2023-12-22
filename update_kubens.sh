db1086c987d1779b6c418fd72d57706616547a4c
Sierra1011 <43705549+Sierra1011@users.noreply.github.com>
2023-12-22 22:50:39 +0000

    add kubectx/ns script

--- update_kubens.sh
+++ update_kubens.sh
@@ -2,11 +2,20 @@
 CURRENT=$(kubectx --version)
 LATEST=$(curl https://api.github.com/repos/ahmetb/kubectx/releases/latest | jq --raw-output '.tag_name' | cut -c 2-)
 if [ "$CURRENT" != "$LATEST" ]; then
-   echo "Installing Kubectx ${LATEST}..."
-   wget -P ~ https://github.com/ahmetb/kubectx/releases/download/v${LATEST}/kubectx_v${LATEST}_linux_x86_64.tar.gz
-   sudo tar -C /usr/local/bin -xvf ~/kubectx_v${LATEST}_linux_x86_64.tar.gz
-   wget -P ~ https://github.com/ahmetb/kubectx/releases/download/v${LATEST}/kubens_v${LATEST}_linux_x86_64.tar.gz
-   sudo tar -C /usr/local/bin -xvf ~/kubens_v${LATEST}_linux_x86_64.tar.gz
+	echo "Installing Kubectx ${LATEST}..."
+	NAME="""kubectx_v"$LATEST"_linux_amd64.tar.gz"""
+        FILTER=".assets[] | select(.\"name\"==\"$NAME\").browser_download_url"
+        BDURL=$(curl https://api.github.com/repos/ahmetb/kubectx/releases/latest | jq -r "$FILTER")
+        wget -P ~/ $BDURL
+	tar -C ~/.bin -xvf ~/kubectx_v${LATEST}_linux_x86_64.tar.gz
+        echo "Kubectx v${LATEST} installed."
+        echo "Installing Kubens ${LATEST}..."
+	NAME="""kubens_v"$LATEST"_linux_amd64.tar.gz"""
+        FILTER=".assets[] | select(.\"name\"==\"$NAME\").browser_download_url"
+        BDURL=$(curl https://api.github.com/repos/ahmetb/kubectx/releases/latest | jq -r "$FILTER")
+        wget -P ~/ $BDURL
+	tar -C ~/.bin -xvf ~/kubens_v${LATEST}_linux_x86_64.tar.gz
+	echo "Kubens v${LATEST} installed."
 else
-   echo "Latest Kubectx/Kubens already installed ($LATEST)."
+	echo "Latest Kubectx/Kubens already installed ($LATEST)."
 fi
